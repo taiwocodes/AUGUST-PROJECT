a874e56adbd7ff62ba75ac6b81640da00b4e3a45
Ruqayat <yinkaajibade97@gmail.com>
2021-06-29 21:38:34 +0100

    changes from ruqayat

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,69 +2,43 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function Navigation(props) {
-  return (
-   
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container">
-          
-            <h1>Team August Driver's Licence App</h1>
-          
-          <div>
-            <ul class="navbar-nav ml-auto">
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/registration" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/registraion">
-                  Registration
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/renewal" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/renewal">
-                  Renewal
-                </Link>
-              </li>
+	return (
 
-               
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/gallery" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/gallery">
-                  Gallery
-                </Link>
-              </li>
-               
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/applicationstatus" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/applicationstatus">
-                  ApplicationStatus
-                </Link>
-              </li>
-              
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/complainsuggestion" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/complainsuggestion">
-                  ComplainSuggestion
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-  );
+		<nav className="navbar">
+			<h1>Team August Driver's Licence App</h1>
+			<ul className="nav-list-container">
+				<li>
+					<Link class="nav-link" to="/registraion">
+						DL Application
+					</Link>
+				</li>
+				<li>
+					<Link class="nav-link" to="/renewal">
+						Renewal
+					</Link>
+				</li>
+
+				<li>
+					<Link class="nav-link" to="/gallery">
+						Gallery
+					</Link>
+				</li>
+
+				<li>
+					<Link class="nav-link" to="/applicationstatus">
+						ApplicationStatus
+					</Link>
+				</li>
+
+				<li>
+					<Link class="nav-link" to="/feedback">
+						Complaints/Suggestions
+					</Link>
+				</li>
+			</ul>
+
+		</nav>
+	);
 }
 
 export default Navigation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import '../styles/home.css';
 
 import Footer from '../components/Footer';
 import React from "react";
+import Navigation from '../components/Navigation';
 
 function Home() {
   return (
         <>
-
+            <Navigation/>
             <div className="background-image-container"></div>
 
             <br/>
