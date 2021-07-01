291f73e517963c313a9a1c0f108a7aa3147decee
Ruqayat <yinkaajibade97@gmail.com>
2021-07-01 09:32:03 +0100

    changes from ruqayat

--- src/pages/FeedbackAccepted.jsx
+++ src/pages/FeedbackAccepted.jsx
@@ -11,7 +11,7 @@ export default function FeedbackAccepted() {
                 width='250px'
             />
             <h1>Thank you for contacting us!</h1>
-            <h2>We will get back to you soon.</h2>
+            <h2>We will get back to you.</h2>
             <Link to='/home' className='home-pg-link'>Go back to home page</Link>
         </div>
     )
